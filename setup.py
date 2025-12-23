520d1963ba0a880bdfdab599ed0c079b54835d99
kanchanasubramani <kanchanasubramani07@gmail.com>
2025-12-23 19:13:27 +0530

    #set up commits

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="ML-PROJECTS",
+    version="1.0.0",
+    author="Kanchana",
+    author_email="kanchanasubramani07@gmail.com",
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'scikit-learn', 'flask','seaborn'],
+)
